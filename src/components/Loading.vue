7d088483c9e0ac98f3378de023bf9aa6fb987f75
cloh0007 <cloh0007@student.monash.edu>
2021-02-22 19:11:09 +0800

    feat(loading): added spinner ui

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,8 +1,10 @@
 <template>
   <!-- Loading screen -->
   <div class="loading__center">
-      <div class="heading">Loading..</div>
+      <div class="heading">Loading</div>
+      <div class="ring"><div></div><div></div><div></div><div></div></div>
   </div>
+  
 </template>
 
 <script>
@@ -17,6 +19,49 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+}
+
+.ring {
+  display: flex;
+  flex-direction: column;
+	align-items: center;
+	justify-content: center;
+  position: relative;
+  width: 80px;
+  height: 70px;
+  
+}
+.ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #000;
+  border-radius: 50%;
+  animation: ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #000 transparent transparent transparent;
+}
+.ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
