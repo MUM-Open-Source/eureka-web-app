f4ad2a9cd002a6bb4ff5452cf508f0dac29bdd65
NitinMa2 <nitinmathew.official@gmail.com>
2021-02-23 16:55:32 +0800

    refactor(loading): nesting the css classes

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -33,28 +33,27 @@ export default {
   position: relative;
   width: 80px;
   height: 70px;
-  
-}
-.ring div {
-  box-sizing: border-box;
-  display: block;
-  position: absolute;
-  width: 64px;
-  height: 64px;
-  margin: 8px;
-  border: 8px solid #000;
-  border-radius: 50%;
-  animation: ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  border-color: #000 transparent transparent transparent;
-}
-.ring div:nth-child(1) {
-  animation-delay: -0.45s;
-}
-.ring div:nth-child(2) {
-  animation-delay: -0.3s;
-}
-.ring div:nth-child(3) {
-  animation-delay: -0.15s;
+  div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border: 8px solid #000;
+    border-radius: 50%;
+    animation: ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #000 transparent transparent transparent;
+    &:nth-child(1) {
+      animation-delay: -0.45s;
+    }
+    &:nth-child(2) {
+      animation-delay: -0.3s;
+    }
+    &:nth-child(3) {
+      animation-delay: -0.15s;
+    }
+  }  
 }
 @keyframes ring {
   0% {
