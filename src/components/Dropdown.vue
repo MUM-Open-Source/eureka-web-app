9791b8d11f057a8c989e69fc20ab3507afa921c2
julkhong <officialkhong@gmail.com>
2021-02-28 11:57:34 +0800

    fixing(profileMenu):removing the block of codes for 'clicking outside' attempt

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -48,19 +48,7 @@ export default {
                 subMenu.style.display = "none";
             }
         }
-        // Close the dropdown menu if the user clicks outside of it
-        window.onclick = function(event) {
-        if (!event.target.matches('.dropbtn')) {
-            var dropdowns = document.getElementsByClassName("dropdown-content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-            }
-        }
-        }
+
 
            return{
                checkSubMenu,
