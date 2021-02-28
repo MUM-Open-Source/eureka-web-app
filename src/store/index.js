b3bf615fc35d43d97a2c09e2e16f4b5b1d7c6e37
cloh0007 <cloh0007@student.monash.edu>
2021-03-01 00:02:00 +0800

    feat(filter): new filter branch & added filter values

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,9 @@ import Swal from 'sweetalert2';
 
 export default createStore({
   // application-level data
+ 
+
+
   state: {
     user: auth.currentUser,               // firebase auth user
     isSideNavCollapsed: true,             // bool to check if sidenav is showing
@@ -16,7 +19,26 @@ export default createStore({
     talent:[],
     mentors: [],
     liked_events: [],                     // list of events liked by the user
-    user_waves: []                        // list of users waved at by the auth user
+    user_waves: [],                       // list of users waved at by the auth user
+    filters: {
+      event:{
+        type:[],
+        organizer:[],
+        name: [],
+      },
+      talent: {
+        interests: [],
+        experience_level: [],
+        background:[],
+        full_name: [],
+      },
+      mentor: {
+        skill: [],
+        experience_level: [],
+        background: [],
+        full_name: [],
+      }
+    },
   },
 
   // functions that affect the state
@@ -117,8 +139,22 @@ export default createStore({
           this.commit('GET_LIKED_EVENTS');
           // update state
           querySnapshot.forEach((doc) => {
+            if (!state.filters.event.type.includes(doc.data().type)) {
+              // push the value into the array
+              state.filters.event.type.push(doc.data().type)
+            }
+            if (!state.filters.event.organizer.includes(doc.data().organizer)) {
+              // push the value into the array
+              state.filters.event.organizer.push(doc.data().organizer)
+            }
+
+            if (!state.filters.event.name.includes(doc.data().name)) {
+              // push the value into the array
+              state.filters.event.name.push(doc.data().name)
+            }
             state.events.push(doc.data());
           });
+          // console.log(state.filters.event);
         })
         .catch(function(error) {
           console.log("Error getting document:"+ error)
@@ -160,8 +196,24 @@ export default createStore({
           // fetch users that auth user waved at
           this.commit('GET_USER_WAVES');
           querySnapshot.forEach((doc) => {
+            
+            //populate values into array
+            if (!state.filters.talent.interests.includes(doc.data().interests)) {
+              state.filters.talent.interests.push(doc.data().interests)
+            }
+            if (!state.filters.talent.experience_level.includes(doc.data().experience_level)) {
+              state.filters.talent.experience_level.push(doc.data().experience_level)
+            }
+            if (!state.filters.talent.background.includes(doc.data().background)) {
+              state.filters.talent.background.push(doc.data().background)
+            }
+            if (!state.filters.talent.full_name.includes(doc.data().full_name)) {
+              state.filters.talent.full_name.push(doc.data().full_name)
+            }
+            
             state.talent.push(doc.data());
           });
+          // console.log(state.filters.talent);
         })
         .catch(function(error) {
           console.log("Error getting document:", error);
@@ -177,8 +229,27 @@ export default createStore({
           // fetch users that auth user waved at
           this.commit('GET_USER_WAVES');
           querySnapshot.forEach((doc) => {
+
+            //populate values into array
+            if (!state.filters.mentor.skill.includes(doc.data().skill)) {
+              state.filters.mentor.skill.push(doc.data().skill)
+            }
+
+            if (!state.filters.mentor.experience_level.includes(doc.data().experience_level)) {
+              state.filters.mentor.experience_level.push(doc.data().experience_level)
+            }
+
+            if (!state.filters.mentor.background.includes(doc.data().background)) {
+              state.filters.mentor.background.push(doc.data().background)
+            }
+
+            if (!state.filters.mentor.full_name.includes(doc.data().full_name)) {
+              state.filters.mentor.full_name.push(doc.data().full_name)
+            }
+            
             state.mentors.push(doc.data());
           });
+          // console.log(state.filters.talent);
         })
         .catch(function(error) {
           console.log("Error getting document:", error);
@@ -360,6 +431,7 @@ export default createStore({
       });
     },
 
+
   },
 
 
@@ -412,8 +484,12 @@ export default createStore({
 
     sendFeedback({ commit }, feedback) {
       commit('SEND_FEEDBACK', feedback);
+    },
+
+    resetPassword({ commit }, email) {
+      commit('RESET_PASSWORD', email);
     }
 
   }
 
-})
+});
