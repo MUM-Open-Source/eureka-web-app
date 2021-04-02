0f72a92735c415e24965db21ec1f548826a30d31
julkhong <officialkhong@gmail.com>
2021-04-02 11:31:43 +0800

    just testing you can ignore this

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <div class="heading heading--normal">Your Go-To Platform to</div>
       <div class="heading mar__b--2">Network with Monash Talent</div>
       <div class="body home__subtitle mar__t--2 mar__b--2">
-        Look no further if you need to find others within the Monash SOIT Community with whom you can team up,
+        LOOK NO FURTHER if you need to find others within the Monash SOIT Community with whom you can team up,
         seek guidance or just simply say hi. We aim to make it easier to find people to join you on your unique
         adventures.
       </div>
