f9ee9f5c57b01f26a1c7503126359a42896eb4f6
CheeMinHao <cheemh3456@gmail.com>
2021-04-03 16:28:30 +0800

    error 404 UI change

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -3,7 +3,7 @@
         <img src="@/assets/not-found-icon.svg" class="pad__b--1"/>
         <div class="">
             <div class="heading pad__b--1">Eu-rrer 404: Not Found</div>
-            <div class="body">Eure- wait. Something went wrong! Try another way, for the Eureka path is not easy.</div>
+            <div class="body">Everything is wrong, ur life is a disaster. Go back to home page.</div>
         </div>
     </div>
 </template>
