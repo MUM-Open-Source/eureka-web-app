d54e0f7e04785d3bda113b8b3612a9a2b65051ba
Chee Min Hao <65438002+CheeMinHao@users.noreply.github.com>
2021-06-23 23:19:18 +0800

    initiall commit for testing

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+// jest.config.js
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+    preset: "@vue/cli-plugin-unit-jest"
+}
---
+++ src/tests/unit/TopNav.spec.js
@@ -0,0 +1,8 @@
+import TopNav from '@/modules/navigation/TopNav'
+const compiler = require('vue-template-compiler')
+
+describe('TopNav', () => {
+    test('check check', () => {
+        expect(isUserMenuShown).toBe(true)
+    })
+})
