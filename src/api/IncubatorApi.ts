fcf65c2b00b53f9e1692ad1968930f1c64663864
Blaise1030 <blaisetiong1030@gmail.com>
2021-07-12 23:08:25 +0800

    Added crud funtions to the Incubator

--- src/api/IncubatorApi.ts
+++ src/api/IncubatorApi.ts
@@ -1,6 +1,8 @@
 import {db} from "../firebase";
 import {Incubator} from "../types/Incubator";
 
+const INCUBATOR_PATH = "incubator";
+
 const workspaceCodeGenerator = () => {
   const code = Math.floor(new Date().getTime() / 1000);
   return code.toString(36);
@@ -8,16 +10,31 @@ const workspaceCodeGenerator = () => {
 
 export const createWorkSpace = (workshopSettings: Incubator) => {
   const code = workspaceCodeGenerator();
-  db.collection("incubator")
+  db.collection(INCUBATOR_PATH)
     .doc(code)
-    .set({...workshopSettings, code})
-    .then((data: any) => {
+    .set({...workshopSettings, code});
+};
+
+export const getWorkspace = (workspaceCode: string) => {
+  db.collection(INCUBATOR_PATH)
+    .doc(workspaceCode)
+    .get()
+    .then((data) => {
       console.log(data);
     });
 };
 
+export const updateWorkspace = (
+  workspaceCode: string,
+  updatedData: {[key: string]: any}
+) => {
+  db.collection(INCUBATOR_PATH)
+    .doc(workspaceCode)
+    .update(updatedData);
+};
+
 export const deleteWorkspace = (workspaceCode: string) => {
-  db.collection("incubator")
+  db.collection(INCUBATOR_PATH)
     .doc(workspaceCode)
     .delete();
 };
