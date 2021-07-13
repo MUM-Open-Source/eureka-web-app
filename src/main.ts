ca766fe31f23944b958d69ef72169963b93a288f
Blaise1030 <blaisetiong1030@gmail.com>
2021-07-13 23:03:31 +0800

    Added Vuelidate to the root

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from '@/store'
+import {createApp} from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "@/store";
+import Vuelidate from "vuelidate";
+
 // import { db, auth } from "@/firebase";
 
 // const db = require('@/firebase')
 // const auth = require('@/firebase')
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(Vuelidate as any)
+  .use(store)
+  .use(router)
+  .mount("#app");
