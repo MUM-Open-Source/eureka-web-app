abef201d26a0373fd263c2213771a5fb94960eec
NitinMa2 <nitinmathew.official@gmail.com>
2021-07-24 01:05:34 +0800

    feat(docs): adding vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'Eureka Developer Docs',
+    themeConfig: {
+        nav: [
+            { text: 'Guide', link: '/' },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/MUM-Open-Source/eureka-web-app',
+            },
+        ],
+        sidebar: [{ text: 'Our Story', link: '/' }],
+    },
+};
---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# Hello VitePress
+
+:tada: :100:
