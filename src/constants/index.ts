9ff1e19037233783fc0101b08422bb3939ae8477
Blaise1030 <blaisetiong1030@gmail.com>
2021-08-21 12:32:06 +0800

    Added constants folder

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+// Roles
+const ROLE_TALENT = 'talent';
+const ROLE_MENTOR = 'mentor';
+const ROLE_ADMIN = 'admin';
+const ROLE_STUDENT = 'student';
+const ROLE_STAFF = 'staff';
+const ROLE_PUBLIC = 'public';
